3f78d93426b83d2e4a18e1f4109b8922514902ba
junkai30 <junkai30@hotmail.com>
2020-06-22 10:47:20 +0000

    Initial commit

---
+++ sw.js
@@ -0,0 +1,65 @@
+let cacheName = 'my-resume-01';
+// let caches;
+let filesToCache = [
+  "images/page-bg.jpg",
+  "images/body-bg.png",
+  "images/sm-facebook-icon.png",
+  "images/sm-twitter-icon.png",
+  "images/sm-linkedin-icon.png",
+  "images/icon-user.png",
+  "images/icon-mail.png",
+  "images/icon-message.png",
+  "images/divider.png",
+  "programming/html.png",
+  "programming/java.png",
+  "programming/python.png",
+  "programming/R.png",
+  "dp2.jpg",
+  "logo.png",
+  "Neo Jun Kai_Resume.docx",
+  "nus.png",
+  "registerServiceWorker.js",
+  "script.js",
+  "SEM 1 TRANSCRIPT.pdf",
+  "srjc.png",
+  "style.css",
+  "manifest.json"
+]
+
+/* 
+start the service worker, when the user access
+the website online. This will add the all the files 
+listed in filesToCache to the browser cache.
+
+*/
+self.addEventListener('install', function(e){
+  console.log("on install")
+    console.log(cacheName);
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache){
+      console.log("Adding files to cache")
+      return cache.addAll(filesToCache)
+    })
+  )
+})
+
+/*
+If offline or if the file exists in the cache, then it will fetch the files from cache
+*/
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request,{
+        cacheName: cacheName
+    }).then(function(response){
+        console.log(response);
+        console.log("Fetching "+e.request.url);
+      return response || fetch (e.request)
+    })
+  )
+})
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
